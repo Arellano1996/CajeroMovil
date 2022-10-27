20aeb3037bb0213ed685f666b576579a5821ad14
Arturo <arturocervantez27@gmail.com>
2022-10-26 22:28:59 -0500

    Pantallas Login y Register

---
+++ MVVM/Views/Login.xaml.cs
@@ -0,0 +1,11 @@
+using CajeroMovil.MVVM.ViewModels;
+namespace CajeroMovil.MVVM.Views;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+		BindingContext = new LoginViewModel();
+	}
+}
