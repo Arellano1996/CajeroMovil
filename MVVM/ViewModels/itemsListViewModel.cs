a0acd46740a7c1510901727f9e47b933b2190505
Luis Arellano <arellano9627@gmail.com>
2022-10-28 13:06:36 -0500

    Test Bug

--- MVVM/ViewModels/itemsListViewModel.cs
+++ MVVM/ViewModels/itemsListViewModel.cs
@@ -1,10 +1,7 @@
-﻿
-
-using CajeroMovil.MVVM.Models;
+﻿using CajeroMovil.MVVM.Models;
 using CajeroMovil.MVVM.Views;
 using PropertyChanged;
 using System.Collections.ObjectModel;
-using System.Text.Json;
 using System.Windows.Input;
 
 namespace CajeroMovil.MVVM.ViewModels
@@ -18,30 +15,34 @@ namespace CajeroMovil.MVVM.ViewModels
         {
             Items = new ObservableCollection<Item>();
         }
-        public ICommand ClickCommand => new Command(Alert);
-        public ICommand DeleteCommand =>
-            new Command( (p) => 
-            {
-                if (Items.Count == 1)
-                {
-                    Items = new ObservableCollection<Item>();
-                }else Items.Remove( (Item)p );
-            });
-        public ICommand ToQRCommand =>
-            new Command(async () => 
-            {
-                await AppShell.Current.GoToAsync(nameof(QRScan));
-            });
-        async public void Alert() 
+        public ICommand ToPayCommand => 
+            new Command( async()=>
         {
             //Items.Add(new Item { id = 3, name = "Item agregado", price = 500, description = "hola", linkImg = "dotnet_bot.svg" });
+
             if (Items.Count == 0)
             {
                 App.Current.MainPage.DisplayAlert("Lista vacía", "Aún no has agregado ningún artículo.", "Ok");
                 return;
             }
             await AppShell.Current.GoToAsync(nameof(Pagar));
-        }
+        });
+        public ICommand ToQRCommand =>
+            new Command(async () =>
+            {
+                await AppShell.Current.GoToAsync(nameof(QRScan));
+            });
+        public ICommand DeleteCommand =>
+            new Command( (p) => 
+            {
+                /*
+                if (Items.Count == 1)
+                {
+                    Items = new ObservableCollection<Item>();
+                }else Items.Remove( (Item)p );*/
+                if (Items.Count > 0) Items.Remove((Item)p);
+            });
+        
         public void AddItem(Item i)
         {
             Items.Add(i);
