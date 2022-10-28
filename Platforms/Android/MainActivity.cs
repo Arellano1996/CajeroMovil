fe63eb4900fa804d8f8a5e6374b1ea27b186e823
Luis Arellano <arellano9627@gmail.com>
2022-10-27 19:01:37 -0500

    Nombre, Icono y Splash Agregado

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -3,8 +3,9 @@ using Android.Content.PM;
 using Android.OS;
 
 namespace CajeroMovil;
-
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+//Agregar nombre Label
+//Agregar Icono Icon
+[Activity(Label ="Cajero Movil", Icon = "@mipmap/splash2", Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
